02342e3fa95826d9c813a1e6e3163eb1ede82bb0
eunjeong Jang <cs801458j@gmail.com>
2018-01-19 00:45:56 -0800

    make resource related files

--- UI/src/dashboard/settings/settings.js
+++ UI/src/dashboard/settings/settings.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import Style from './settings.scss'
 import TimezoneContainer from './timezoneContainer'
+import ResourceContainer from './resourceContainer'
 
 export default class Settings extends Component {
     render() {
@@ -17,10 +18,18 @@ export default class Settings extends Component {
                                     <img src='img/ic_access_time_white.svg'/>
                                     <div className={Style.text}>Timezone</div>
                                 </div>
+                                <div>
+                                    <img src='img/ic_access_time_white.svg'/>
+                                    <div className={Style.text}>Resource</div>
+                                </div>
                             </div>
                         </nav>
                         <div className={Style.content}>
                             <TimezoneContainer settingContainer={this.props.settingContainer} dashBoardContainer={this.props.dashBoardContainer} app={this.props.app}/>
+                        </div>
+                        //  수정중인 부
+                        <div className{Style.content}>
+                            <ResourceContainer settingContainer={this.props.settingContainer} dashBoardContainer={this.props.dashBoardContainer} app={this.props.app}/>
                         </div>
                     </section>
             </section>
